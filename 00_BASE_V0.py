875a952755286e16aa14d15aac180b22404e293e
ClioSugar06 <107890642+ClioSugar06@users.noreply.github.com>
2022-06-21 09:25:25 +1200

    Skeleton of base code

---
+++ 00_BASE_V0.py
@@ -0,0 +1,14 @@
+'''
+base component for the game lucky unicorn
+Version 0, skeleton
+
+Author Cariad Anglesey
+CC CA 2022
+'''
+
+# import libraries below this line ******************
+
+# All functions below this line  ******************
+
+# main routine below this line ******************
+print("hello")
